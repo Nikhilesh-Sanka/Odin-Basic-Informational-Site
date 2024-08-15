a58532c61e6247c2ed82ccdca86aab5181afb02a
Nikhilesh-Sanka <sankanikhilesh2006.svc@gmail.com>
2024-08-15 19:09:03 +0530

    Did Something with the fucking node ...

---
+++ app.js
@@ -0,0 +1,34 @@
+const http = require("http");
+const fs = require("node:fs");
+
+http
+  .createServer((req, res) => {
+    let path = req.url;
+    res.writeHead(400, { "Content-Type": "text/html" });
+    if (path === "/index") {
+      fs.readFile("./index.html", (err, data) => {
+        if (err) throw err;
+        res.write(data);
+        res.end();
+      });
+    } else if (path === "/contact") {
+      fs.readFile("./contact.html", (err, data) => {
+        if (err) throw err;
+        res.write(data);
+        res.end();
+      });
+    } else if (path === "/about") {
+      fs.readFile("./about.html", (err, data) => {
+        if (err) throw err;
+        res.write(data);
+        res.end();
+      });
+    } else {
+      fs.readFile("./404.html", (err, data) => {
+        if (err) throw err;
+        res.write(data);
+        res.end();
+      });
+    }
+  })
+  .listen(8080);
